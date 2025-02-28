ae813ac9091b2a4eff24436444ff0290b03248ee
fui <fui@fui-ubuntu>
2025-02-27 19:18:34 -0600

    Implemented the hash class

--- src/main/java/edu/grinnell/csc207/blockchain/Hash.java
+++ src/main/java/edu/grinnell/csc207/blockchain/Hash.java
@@ -1,8 +1,76 @@
 package edu.grinnell.csc207.blockchain;
 
+import java.util.Arrays;
+
+
 /**
  * A wrapper class over a hash value (a byte array).
  */
 public class Hash {
-    // TODO: fill me in!
+    
+    private final byte[] hash;
+    
+    /**
+     * creates a new Hash object with the data as the hash
+     * @param data 
+     */
+    public Hash(byte[] data){
+        this.hash = data;
+        
+    }
+    
+    /**
+     * 
+     * @return the hash value as a byte[]
+     */
+    public byte[] getData(){
+        return this.hash;
+    }
+    
+    /**
+     * 
+     * @return true if the conditions for hash validity met, false if not
+     */
+    public boolean isValid(){
+        
+        for (int i = 0; i < 3; i++){
+            if (this.hash[i] != 0){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+
+    @Override
+    public String toString(){
+    
+        String hashString = "";
+        for (int i = 0; i < this.hash.length; i++) {
+            hashString += String.format("%d", Byte.toUnsignedInt(this.hash[i]));
+        }
+        return hashString;
+    }
+    
+    /**
+     * 
+     * @param other
+     * @return returns true if this hash is structurally equal to the argument.
+     * 
+     * if other is type Hash, then cast hash onto other and compare the values of other.hash and this.hash
+     */
+    @Override
+    public boolean equals(Object other){
+        
+        if (other instanceof Hash){
+            
+            Hash otherHash = (Hash) other;     
+            return Arrays.equals(this.hash, otherHash.hash);
+            
+        } else {
+            
+            return false;
+        }
+    }
 }
