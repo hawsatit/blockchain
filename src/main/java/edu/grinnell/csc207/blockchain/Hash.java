ca4e5ffc7fc08bc76e92c2d76821c49e0f05a923
fui <fui@fui-ubuntu>
2025-04-07 09:48:01 -0500

    Made resubmission changes

--- src/main/java/edu/grinnell/csc207/blockchain/Hash.java
+++ src/main/java/edu/grinnell/csc207/blockchain/Hash.java
@@ -2,7 +2,6 @@ package edu.grinnell.csc207.blockchain;
 
 import java.util.Arrays;
 
-
 /**
  * A wrapper class over a hash value (a byte array).
  */
@@ -11,65 +10,58 @@ public class Hash {
     private final byte[] hash;
     
     /**
-     * creates a new Hash object with the data as the hash
-     * @param data 
+     * Creates a new Hash object with the data as the hash.
+     * 
+     * @param data the byte array representing the hash value
      */
-    public Hash(byte[] data){
+    public Hash(byte[] data) {
         this.hash = data;
-        
     }
     
     /**
+     * Returns the hash value as a byte array.
      * 
      * @return the hash value as a byte[]
      */
-    public byte[] getData(){
+    public byte[] getData() {
         return this.hash;
     }
     
     /**
+     * Checks if the hash is valid according to specific conditions.
      * 
-     * @return true if the conditions for hash validity met, false if not
+     * @return true if the conditions for hash validity are met, false otherwise
      */
-    public boolean isValid(){
-        
-        for (int i = 0; i < 3; i++){
-            if (this.hash[i] != 0){
+    public boolean isValid() {
+        for (int i = 0; i < 3; i++) {
+            if (this.hash[i] != 0) {
                 return false;
             }
         }
-        
         return true;
     }
     
-
     @Override
-    public String toString(){
-    
-        String hashString = "";
+    public String toString() {
+        StringBuilder hashString = new StringBuilder();
         for (int i = 0; i < this.hash.length; i++) {
-            hashString += String.format("%02x", Byte.toUnsignedInt(this.hash[i]));
+            hashString.append(String.format("%02x", Byte.toUnsignedInt(this.hash[i])));
         }
-        return hashString;
+        return hashString.toString();
     }
     
     /**
+     * Compares this hash with another object for structural equality.
      * 
-     * @param other
-     * @return returns true if this hash is structurally equal to the argument.
-     * 
-     * if other is type Hash, then cast hash onto other and compare the values of other.hash and this.hash
+     * @param other the object to compare to
+     * @return true if this hash is structurally equal to the argument, false otherwise
      */
     @Override
-    public boolean equals(Object other){
-        
-        if (other instanceof Hash){
-            
+    public boolean equals(Object other) {
+        if (other instanceof Hash) {
             Hash otherHash = (Hash) other;     
             return Arrays.equals(this.hash, otherHash.hash);
-            
         } else {
-            
             return false;
         }
     }
